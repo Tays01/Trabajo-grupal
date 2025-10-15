c8a1efa7bae190a4550f1b4da15f4cc7604b95b5
Lliliana370 <lc8578017@gmail.com>
2025-10-15 15:47:20 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 git add index.php
-git commit -m "feat: add index.php con h1 y div inline styles - Pamela"
+git commit -m "feat: add index.php con h1 y div inline styles - Liliana"
 git push origin main
