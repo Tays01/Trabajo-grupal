b59d7292697a723f6eb3da5a9fdf5d12a68dc471
POLK BRANDO VERNAZA QUIÑONEZ <83431658+o698polk@users.noreply.github.com>
2025-10-15 15:58:58 -0500

    CAMBIOS REALZIADOS POR POLK

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-git add index.php
-git commit -m "feat: add index.php con h1 y div inline styles - Liliana"
-git push origin main
+<DOCTYPE html>
+<html>
+    <head>
+    <title>PHP</title>
+    </head>
+    <body>
+    <h1>HOLA SOY POLK</h1>
+    </body>
+    </html>
