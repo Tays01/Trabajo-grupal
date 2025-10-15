69fbee17b257a7342bfc2211db01a8c303da4e5c
Jocelyn Tatiana González Meza <jocetaty26@gmail.com>
2025-10-14 22:04:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 git add index.php
-git commit -m "feat: add index.php con h1 y div inline styles - nombreAlumno"
+git commit -m "feat: add index.php con h1 y div inline styles - Jocelyn"
 git push origin main
