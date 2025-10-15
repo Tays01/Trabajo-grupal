11e62e4c4a8980bf161bc145372b8bfa3049232d
pamela387 <pamelacasti1993@gmail.com>
2025-10-15 15:41:20 -0500

    feat: add index.php con h1 y div inline styles - Jocelyn

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 git add index.php
-git commit -m "feat: add index.php con h1 y div inline styles - Jeremy"
+git commit -m "feat: add index.php con h1 y div inline styles - Pamela"
 git push origin main
